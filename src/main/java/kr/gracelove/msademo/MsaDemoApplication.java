2893f82c9af08a15e672d95a3d866a76c9f662cd
Grace <govlmo91@gmail.com>
2020-09-10 00:23:26 +0900

    init

---
+++ src/main/java/kr/gracelove/msademo/MsaDemoApplication.java
@@ -0,0 +1,13 @@
+package kr.gracelove.msademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaDemoApplication.class, args);
+	}
+
+}
